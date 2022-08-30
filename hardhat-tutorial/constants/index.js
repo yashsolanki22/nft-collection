05a2865a8c69f4068d8d0eb06c30f4a5b74ece88
Yash <ysolanki069@gmail.com>
2022-08-30 23:04:56 +0530

    updated the url

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x73c74c077da1aF949d7EA9c825baB8F1F82b0dCC";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nftcollection-dapp-goerli.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
